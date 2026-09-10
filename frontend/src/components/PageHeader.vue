df82a1cd12f50d9b37646eb1cb0e9057478d45a6
konni <104560647+konnisan@users.noreply.github.com>
2026-09-10 16:36:25 +0800

    ui: open up page headers

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -7,7 +7,7 @@ defineProps({
 
 <template>
   <header class="page-heading">
-    <div>
+    <div class="page-heading-copy">
       <h1>{{ title }}</h1>
       <p v-if="description">{{ description }}</p>
     </div>
